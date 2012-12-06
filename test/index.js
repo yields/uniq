241e73da8e9c80fc0cd09929df610c40f5f6f3f5
Damian Suarez <rdsuarez@gmail.com>
2012-12-06 14:25:09 -0300

    test: add `select` test

--- test/index.js
+++ test/index.js
@@ -1,8 +1,13 @@
 var uniq = require('../');
 
 describe('uniq(array)', function(){
-  it('should remove duplicated items from array', function(){
+  it('should remove repeated items', function(){
     var a = [1, 2, 3, 3, null, null, undefined, undefined, 'a', 'a', false, false];
     uniq(a).should.be.eql([1, 2, 3, null, undefined, 'a', false]);
+  });
+
+  it('should remove specifics repeated items', function(){
+    var a = [1, 2, 3, 1, 2, 3, 1, 2, 3];
+    uniq(a, [1, 3]).should.be.eql([1, 2, 3, 2, 2]);
   });
 });
